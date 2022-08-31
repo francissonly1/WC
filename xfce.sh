5860ad8e6a901d6497c1781c62309e49175ca01f
francissonly1 <35592462+francissonly1@users.noreply.github.com>
2022-08-30 20:07:51 -0400

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,16 +1,13 @@
 echo "Download ngrok"
 wget https://raw.githubusercontent.com/akuhnet/w-colab/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
-echo Downloading File From akuh.net
-echo "===================================="
-echo "Wait 2 Minutes"
-echo "This is only for education"
-echo "Other interesting tutorials visit akuh.net"
-echo "===================================="
+echo "Downloading Files"
 sudo apt-get update > /dev/null 2>&1
 echo "===================================="
-echo "Install Firefox"
+echo "Installing Browsers..."
 echo "===================================="
 sudo apt install firefox -y > /dev/null 2>&1
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo apt install ./google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
@@ -21,15 +18,13 @@ sudo apt remove --assume-yes gnome-terminal
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 echo "===================================="
-echo "Start RDP"
+echo "Starting RDP..."
 echo "===================================="
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "More  Free VPS akuh.net"
-echo "Don't close this tab RDP runs 12 hours"
-echo "Keep support akuh.net thank you"
+echo "You Can Connect to VPS Now"
 echo "===================================="
 sleep 43210
